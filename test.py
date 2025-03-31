b0c3f372632b9910d25b86520f737f039cbb2ab1
kang <88534778+lorhhhan@users.noreply.github.com>
2025-03-31 17:55:18 +0800

    Update demo_test.py

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 from bert_seq2seq.tokenizer import load_chinese_base_vocab, Tokenizer
 from bert_seq2seq.bert_cls_classifier import BertClsClassifier
 
-# ========= 配置 =========
 vocab_path = "./state_dict/roberta_wwm_vocab.txt"
 model_path = "./sentiment_model.bin"
 test_csv_path = "data/test.csv"
@@ -14,11 +13,11 @@ output_csv_path = "data/test_with_predictions.csv"
 batch_size = 32
 target_size = 2  # 二分类
 
-# ========= 加载词表和 tokenizer =========
+# 加载词表和 tokenizer
 word2idx = load_chinese_base_vocab(vocab_path)
 tokenizer = Tokenizer(word2idx)
 
-# ========= 定义 Dataset =========
+# 定义 Dataset
 class TestDataset(Dataset):
     def __init__(self, texts):
         self.texts = texts
